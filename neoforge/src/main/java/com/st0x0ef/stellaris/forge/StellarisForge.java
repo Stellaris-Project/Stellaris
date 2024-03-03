6dc922e7cde4dc71f5d4c0d738a2c16e26fedb21
TATHAN <costa.tathan@gmail.com>
2024-03-03 16:49:31 +0100

    Fix Neoforge

--- neoforge/src/main/java/com/st0x0ef/stellaris/forge/StellarisForge.java
+++ neoforge/src/main/java/com/st0x0ef/stellaris/forge/StellarisForge.java
@@ -1,12 +1,15 @@
 package com.st0x0ef.stellaris.forge;
 
 import com.st0x0ef.stellaris.Stellaris;
+import dev.architectury.platform.hooks.EventBusesHooks;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
 
 @Mod(Stellaris.MOD_ID)
 public class StellarisForge {
     public StellarisForge() {
         // Submit our event bus to let architectury register our content on the right time
         Stellaris.init();
+
     }
 }
