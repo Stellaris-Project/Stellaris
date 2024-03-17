85550d451228684ee8346e8ab34f1da9503f7320
TATHAN <costa.tathan@gmail.com>
2024-03-17 17:17:33 +0100

    Items Drops

--- common/src/main/java/com/st0x0ef/stellaris/common/blocks/RocketStation.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/blocks/RocketStation.java
@@ -12,10 +12,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResult;
-import net.minecraft.world.MenuProvider;
-import net.minecraft.world.SimpleMenuProvider;
+import net.minecraft.world.*;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.BrewingStandMenu;
 import net.minecraft.world.inventory.ContainerLevelAccess;
@@ -65,5 +62,17 @@ public class RocketStation extends BaseEntityBlock {
     public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level level, BlockState blockState, BlockEntityType<T> blockEntityType) {
         return createTickerHelper(blockEntityType, BlockEntityTypesRegistry.ROCKET_STATION.get(),
                 (world1, pos, state1, blockEntity) -> blockEntity.tick(world1, pos, state1));
+    }
+
+    @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState blockState2, boolean bl) {
+        if (blockState.getBlock() != blockState2.getBlock()) {
+            BlockEntity blockEntity = level.getBlockEntity(blockPos);
+            if (blockEntity instanceof RocketStationEntity) {
+                Containers.dropContents(level, blockPos, (RocketStationEntity)blockEntity);
+                level.updateNeighbourForOutputSignal(blockPos,this);
+            }
+            super.onRemove(blockState, level, blockPos, blockState2, bl);
+        }
     }
 }
