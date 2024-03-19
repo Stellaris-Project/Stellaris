b494606e749c538371d072a37bf0df402c0e479b
TATHAN <costa.tathan@gmail.com>
2024-03-19 22:04:45 +0100

    Config loaded !

--- common/src/main/java/com/st0x0ef/stellaris/common/config/CustomConfig.java
+++ common/src/main/java/com/st0x0ef/stellaris/common/config/CustomConfig.java
@@ -1,10 +1,14 @@
 package com.st0x0ef.stellaris.common.config;
 
+import com.google.common.reflect.TypeToken;
+import com.google.gson.JsonObject;
 import com.st0x0ef.stellaris.Stellaris;
 import dev.architectury.platform.Platform;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -16,20 +20,30 @@ public class CustomConfig {
 
     public static void init() {
         loadConfigFile();
-
-        addEntry();
+        addEntries(CONFIG);
         writeConfigFile(CONFIG, "stellaris.json");
         //config = Stellaris.GSON.fromJson(Platform.getStreamForResource("stellaris.json"), Map.class);
 
     }
 
-    public static void addEntry() {
+    public static void addEntries(Map<Object, ConfigEntry> config) {
 
-        CONFIG.put("test", new ConfigEntry.ConfigEntryInt(1, "test"));
-        CONFIG.put("test2", new ConfigEntry.ConfigEntryString("test", "test"));
-        CONFIG.put("test3", new ConfigEntry.ConfigEntryString("test", "test"));
+        addEntry("test", new ConfigEntry.ConfigEntryInt(1, "caca"), config);
+        addEntry("test2", new ConfigEntry.ConfigEntryInt(1, "caca"), config);
+        addEntry("test3", new ConfigEntry.ConfigEntryInt(1, "caca"), config);
 
-        Stellaris.LOG.error(CONFIG.toString());
+    }
+
+    public static void addEntry(String name, ConfigEntry entry, Map<Object, ConfigEntry> config) {
+
+
+        ConfigEntry test = config.get(name);
+
+
+        if(entry.equals(test)) {
+            return;
+        }
+        config.put(name, entry);
 
     }
 
@@ -44,23 +58,32 @@ public class CustomConfig {
     }
 
     public static void loadConfigFile() {
-        //config = Stellaris.GSON.fromJson(Platform.getStreamForResource("stellaris.json"), Map.class);
-        Iterator<?> keysItr = CONFIG.keySet().iterator();
 
-        Map<Object, ConfigEntry> config = new HashMap<>();
+        String jsonString;
+        HashMap<Object, ConfigEntry> map = new HashMap<>();
 
-        while(keysItr.hasNext()) {
-            String key = (String) keysItr.next();
-            ConfigEntry value = CONFIG.get(key);
+        try {
+            jsonString = readFileAsString(Platform.getConfigFolder() + "/stellaris.json");
+            JsonObject jsonObject = Stellaris.GSON.fromJson(jsonString, JsonObject.class);
 
+            jsonObject.getAsJsonObject().entrySet().forEach(entry -> {
+                String key = entry.getKey();
+                JsonObject value = entry.getValue().getAsJsonObject();
 
-            Stellaris.LOG.error(key + " " + value);
-            config.put(key, value);
+                map.put(key, new ConfigEntry.ConfigEntryString(value.get("value").toString(), value.get("description").getAsString()));
+
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
-        CONFIG = config;
+        CONFIG = map;
+
     }
 
-
+    public static String readFileAsString(String file) throws Exception
+    {
+        return new String(Files.readAllBytes(Paths.get(file)));
+    }
 
 }
